747a738f2cf9bf185b357b536bdeb4d7bce91fc4
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-02-25 22:20:45 +0600

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
 const bodyParser = require('body-parser');
+const port = 3010
 
 const app = express();
 
@@ -11,22 +12,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({ origin: true }));
 
 app.post('/', (req, res) => {
-  
+
   sgMail.setApiKey('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');
 
 
-        const msg = {
-          to: req.body.to,
-          cc: req.body.cc,
-          bcc:req.body.bcc,
-          from: 'app@airpilotbase.com',
-          subject: req.body.subject,
-          text: req.body.text,
-          html: req.body.html,
-        };
+  const msg = {
+    to: req.body.to,
+    cc: req.body.cc,
+    bcc:req.body.bcc,
+    from: 'app@airpilotbase.com',
+    subject: req.body.subject,
+    text: req.body.text,
+    html: req.body.html,
+  };
 
     sgMail.send(msg);
-  
+
 });
 
-exports.sendMail = functions.https.onRequest(app);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
