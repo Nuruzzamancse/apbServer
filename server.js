133b07b27cc3d48209d2e42a926850f550b267d5
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-02-25 22:14:21 +0600

    updated

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+ app.use(bodyParser.json())
+
+app.use(cors({ origin: true }));
+
+app.post('/', (req, res) => {
+  
+  sgMail.setApiKey('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');
+
+
+        const msg = {
+          to: req.body.to,
+          cc: req.body.cc,
+          bcc:req.body.bcc,
+          from: 'app@airpilotbase.com',
+          subject: req.body.subject,
+          text: req.body.text,
+          html: req.body.html,
+        };
+
+    sgMail.send(msg);
+  
+});
+
+exports.sendMail = functions.https.onRequest(app);
