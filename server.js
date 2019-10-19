fce5fbcee936b66822c72032bce1aaffa24b7718
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-10-20 00:49:18 +0600

    deletion operation is working

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
 const port = process.env.PORT || 3010 
 
 const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY
+  }),
+  databaseURL: 'https://apb-dev.firebaseio.com'
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
  app.use(bodyParser.json())
@@ -28,6 +38,17 @@ app.post('/', (req, res) => {
 
     sgMail.send(msg);
 
+});
+
+app.get('/:id', (req, res) => {
+  const userId = req.params.id;
+  admin.auth().deleteUser(userId)
+  .then((user) =>{
+    res.json({success: true, message: `User deleted successfully`})
+  })
+  .catch((error) =>{
+    res.status(400).json({success: false, message: 'Can not delete user'})
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
